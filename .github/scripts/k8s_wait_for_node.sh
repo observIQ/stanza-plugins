4b4d48f8019e1d43d7da30f4bea2a7f4a45e2f46
jsirianni <joe.sirianni@bluemedora.com>
2021-10-09 11:26:57 -0400

    use wait script with trap

---
+++ .github/scripts/k8s_wait_for_node.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eE
+
+NODE="$1"
+
+fail () {
+    kubectl describe nodes
+    kubectl get node "${node}"
+    exit 1
+}
+trap clean ERR
+
+echo "waiting for node: ${node}"
+kubectl get node "${node}"
+kubectl wait --for=condition=Ready --timeout=60s "node/${node}"
