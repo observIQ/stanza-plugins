798e696bc89b22a4fd14d03c46c6734c2770d5ad
jsirianni <joe.sirianni@bluemedora.com>
2022-02-12 14:00:53 -0500

    continue on minikube

--- .github/actions/test-plugin/test-plugin.sh
+++ .github/actions/test-plugin/test-plugin.sh
@@ -10,18 +10,35 @@ k8s="$4"
 k8s_version="$5"
 k8s_runtime="$6"
 
-install_minikube() {
+minikube_install() {
     sudo apt-get update -qq
     sudo apt-get install -qq -y conntrack
     curl -s -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
     sudo install minikube-linux-amd64 /usr/local/bin/minikube
 }
 
-start_mini_kube() {
+minikube_start() {
     minikube start \
         --driver=docker \
         --kubernetes-version="$k8s_version" \
         --container-runtime="$k8s_runtime"
+}
+
+minikube_wait() {
+    node=$(kubectl get node | grep -v NAME | awk '{print $1}')
+    bash ./.github/scripts/k8s_wait_for_node.sh "$node"
+}
+
+minikube_load_image() {
+    minikube image load agent:latest
+}
+
+minikube_create_configmap() {
+    # TODO(jsirianni): Configmap should be dynamic, right now we are hardcoding the input files
+    kubectl create configmap logs \
+        --from-file="log-library/cases/${workflow}/${workflow_case}/logs/adservice-5657f795f5-ql97m_default_server-2180fc6125cb444bd32be19cfa73e71a25e5d6c98b59b5191ee51fc6ff6c6723.log" \
+        --from-file="log-library/cases/${workflow}/${workflow_case}/logs/example-json-logger-6b49dd876-xmr2j_default_logger-ebd483078a72e8c029757f27d872b238ac26e1d0c8c7d842415790c33cc24451.log" \
+        --from-file="log-library/cases/${workflow}/${workflow_case}/logs/productcatalogservice-7cdd54f7c7-nbsgt_default_server-0a3bc17f1a059f850fc35b8b947fcb2f1a7a726b2574f48c973015e49df47e59.log"
 }
 
 install_build_tools() {
@@ -83,13 +100,17 @@ test_empty_output() {
 ### Main ###
 
 if "$k8s"; then
-    install_minikube
-    start_mini_kube
+    minikube_install
+    minikube_start
+    minikube_wait
+    minikube_load_image
+    minikube_create_configmap
 fi
 
 install_build_tools
 build_collector
 build_agent_image
+run_agent
 pause_for_logs
 stop_agent
 dump_agent_logs
