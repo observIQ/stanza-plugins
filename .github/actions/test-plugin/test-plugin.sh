ecf618b14ee7db1e5281d52320d99700982abfb8
jsirianni <joe.sirianni@bluemedora.com>
2022-02-12 13:11:21 -0500

    start building re-usable action

---
+++ .github/actions/test-plugin/test-plugin.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Assumes pwd == git root
+
+set -e
+
+install_build_tools() {
+    echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
+    sudo apt-get update -qq
+    sudo apt-get --no-install-recommends install -qq -y goreleaser
+}
+
+build_collector() {
+    cd observiq-collector
+    make collector
+    cd ../
+}
+
+build_agent_iamge() {
+    docker build . -t agent:latest -f .github/Dockerfile
+}
+
+install_build_tools
+build_collector
